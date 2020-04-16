689a3e16201ef3ae5ee02e2f70ec86f9e8d076f1
Nonelaci Her <nonelaci10@gmail.com>
2020-04-15 21:21:39 -0500

    pushing work in

--- queries.sql
+++ queries.sql
@@ -1,17 +1,28 @@
+DROP TABLE farmers;
+DROP TABLE population;
+
 -- Create Two Tables
 CREATE TABLE farmers (
-  FMID INT PRIMARY KEY,
-  Market_Name VARCHAR,
-  City VARCHAR,
-  STATE VARCHAR,
-  Lat INT,
-  Lon INT
+  id VARCHAR,
+  market_name VARCHAR,
+  city VARCHAR,
+  state VARCHAR,
+  lat INT,
+  lon INT
 );
 
 
 CREATE TABLE population (
-  City_Name VARCHAR,
-  STATE VARCHAR,
-  Population INT
+  city VARCHAR,
+  state VARCHAR,
+  population INT
 );
 
+SELECT market_name, city, state
+FROM farmers f
+RIGHT JOIN population p
+ON f.state = p.state 
+AND f.city = p.city
+;
+
+SELECT * FROM farmers;
